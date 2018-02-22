08659318135c1c41580eb0baf7463d3d5bdea847
Takenori Okashita (GOUTEN) <gouten@5010works.com>
2018-02-22 18:54:43 +0900

    Update.

--- clean-install.sh
+++ clean-install.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
+vagrant destroy
 echo "Reset Default..."
-$ rm -rf .ansible
-$ rm -rf .vagrant
-$ rm -rf provision/playbook.retry
-$ rm -rf wordpress
-$ rm -rf playbook-post.retry
+
+rm -rf .ansible
+rm -rf .vagrant
+rm -rf provision/playbook.retry
+rm -rf wordpress
+rm -rf playbook-post.retry
 
 vagrant up
