f119a1abfd4a14390d01bd47aa11785345ebf92f
GOUTEN <gouten@5010works.com>
2018-02-24 12:23:16 +0900

    Setting Import WordCamp Plugins from SVN.

--- clean-install.sh
+++ clean-install.sh
@@ -3,10 +3,12 @@
 echo "Reset Default..."
 vagrant destroy
 
+echo "Remove files..."
 rm -rf .ansible
 rm -rf .vagrant
 rm -rf provision/playbook.retry
 rm -rf wordpress
 rm -rf playbook-post.retry
 
+echo "Setup wc-vccw..."
 vagrant up
